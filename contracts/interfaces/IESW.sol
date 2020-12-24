19799deb970df651d516e1501b7df41ae24ea5bd
Eugene Rupakov <eugene.rupakov@vnx.io>
2020-12-24 10:03:30 +0300

    Added burn procedures

--- contracts/interfaces/IESW.sol
+++ contracts/interfaces/IESW.sol
@@ -17,4 +17,6 @@ interface IESW {
   function mintAndFreeze(address recipient, uint256 amount, uint256 category) external;
   function mintVirtualAndFreeze(address recipient, uint256 amount, uint256 category) external;
   function mintVirtualAndFreezePresale(address recipient, uint32 sinceDate, uint256 amount, uint256 category) external;
+  function mintClaimed(address recipient, uint256 amount) external;
+  function burn(address account, uint256 amount) external;
 }
